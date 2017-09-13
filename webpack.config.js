93e65c699b31f7b34eccdb09ff6b47ce28b4b110
Jay Sawyer <jaynsawyer@gmail.com>
2017-09-13 03:32:41 -0400

    http-experiment: working webpack-express-typescript boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        publicPath: '',
+        filename: "bundle.js",
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    'awesome-typescript-loader?{tsconfig: "tsconfig.json"}'
+                ]
+            },
+            { test: /\.html$/, loader: 'raw-loader' }
+        ]
+    },
+
+    resolve: {
+        extensions: [ '.ts', '.js', '.json', '.html' ]
+    },
+    
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+          // The (\\|\/) piece accounts for path separators in *nix and Windows
+          path.resolve(__dirname, '../src')
+        ),
+        new HtmlWebpackPlugin({
+          template: './src/index.html'
+        }),
+        new webpack.DefinePlugin({
+          app: {
+            environment: JSON.stringify(process.env.APP_ENVIRONMENT || 'development')
+          }
+        })
+      ]
+
+};
